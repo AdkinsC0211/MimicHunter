a91540d06e415a0d67d666a38765b0f05ab784a8
AndrewCramer3 <97630245+AndrewCramer3@users.noreply.github.com>
2022-09-11 18:21:32 -0400

    Bullets and Bullet Spawners
    Bullets now spawn around an object with the help of the bullet spawner. Bullet Spawners are invisible Area2D objects that can be a child node of mimics and can spawn bullets at random intervals.

--- Code/Bullet.gd
+++ Code/Bullet.gd
@@ -1,16 +1,42 @@
 extends Area2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var bullet_speed = 100
+var bullet_velocity = Vector2(0, 0)
+var bullet_position = Vector2(0, 0)
+export var bullet_time = 10
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+
+# --------------------------------------------------------------
+# Called when the node enters the scene tree for the first time.
+# --------------------------------------------------------------
+# Pass in the movement vector [velocity vector]
+	# Probably a Vector2().normalized() of the bullet spawn point to the spawner origin position
+func set_velocity(velocity):
+	bullet_velocity = velocity
+	
+func set_position(position):
+	bullet_position = position
+	global_translate(bullet_position)
+
+#---------------------------------------------------------------
+# Called every frame [the update() function]
+# --------------------------------------------------------------
+# Update position of the bullet
+# Check if there are any collisions
+# Do any other checks or updates in this function
+func _process(delta):
+	do_movement(delta)
+	if_timer_done()
+	
+func do_movement(delta):
+	global_translate(bullet_velocity * delta * bullet_speed)
+	bullet_position += bullet_velocity
+	
+func if_timer_done():
+	if bullet_time <= 0.0:
+		queue_free()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
