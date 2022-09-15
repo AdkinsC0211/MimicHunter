7fc461d9025e79655928ac0976cc6f3eb8be19bb
Connor Adkins <adkinsc0211@gmail.com>
2022-09-14 21:56:27 -0400

    added temporary hit particle and damage to bullets

--- Code/Bullet.gd
+++ Code/Bullet.gd
@@ -5,6 +5,7 @@ export var bullet_speed = 150
 var bullet_velocity = Vector2(0, 0)
 var bullet_position = Vector2(0, 0)
 export var bullet_time = 10
+var damage = 1
 
 func _ready():
 	pass
@@ -29,12 +30,18 @@ func _process(delta):
 	do_movement(delta)
 	if_timer_done()
 	
+	
+	
 func do_movement(delta):
 	#global_translate(bullet_velocity )
 	position += bullet_velocity * delta * bullet_speed
+	
 	
 func if_timer_done():
 	if bullet_time <= 0.0:
 		queue_free()
 
 
+func _on_bullet_area_body_entered(body):
+	if body.is_in_group("PLAYER"):
+		body.hurt(damage)
