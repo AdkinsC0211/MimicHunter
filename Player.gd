015f7d07d085ab64e6a9fcf86050a0ce30cb7ebc
Elijah <aberjack776@gmail.com>
2022-09-17 15:27:12 -0400

    Cleaned up attack input
    I realized there is no need to have different aim and attack inputs, so not the arrow keys automatically attack

--- Player.gd
+++ Player.gd
@@ -5,6 +5,7 @@ export var speed = 85		#speed is changeable to wtv we need it to be
 var attack_strength: float = 1
 var max_health: float = 10
 var health: float = max_health
+var bonking: bool = false
 
 var current_direction = Vector2.ZERO
 
@@ -20,30 +21,23 @@ var directional_map = {
 func _ready():
 	pass
 	
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("attack"):
-		#temporary code for testing.
-		#this should be bonking in the direction the player is facing
-		bonk(current_direction)
-	elif Input.is_action_just_released("attack"):
-		bonk_over()
-
+	
 #it's best practice to use physics process for character movement
 #it gives you access to functions for the built in physics system
 func _physics_process(delta: float) -> void:
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("walk_right"):
 		velocity.x += 1
-		current_direction = Vector2(1, 0)
+		#current_direction = Vector2(1, 0)
 	if Input.is_action_pressed("walk_left"):
 		velocity.x -= 1
-		current_direction = Vector2(-1, 0)
+		#current_direction = Vector2(-1, 0)
 	if Input.is_action_pressed("walk_down"):
 		velocity.y += 1
-		current_direction = Vector2(0, 1)
+		#current_direction = Vector2(0, 1)
 	if Input.is_action_pressed("walk_up"):
 		velocity.y -= 1
-		current_direction = Vector2(0, -1)
+		#current_direction = Vector2(0, -1)
 	# this direction check method is fine, but I wanted to try
 	#Binding of Isaac style 
 	
@@ -61,8 +55,11 @@ func _physics_process(delta: float) -> void:
 	#binding of Isaac style direction logic
 	#	# checking for a diagonal direction
 	for dir in directional_map:
-		if Input.is_action_pressed(dir):
+		if Input.is_action_just_pressed(dir):
 			current_direction = directional_map[dir]
+			if not bonking:
+				bonk(current_direction)
+			break
 
 
 	if velocity.length() > 0:
@@ -100,6 +97,8 @@ func bonk(direction: Vector2)->void:
 	var offset: Vector2 = direction * 15 # this is seperate in case we change factor
 	var attack_box = $AttackBox
 	var attack_sprite = $AttackSprite
+	bonking=true
+	$BonkTimer.start()
 	attack_box.position = offset
 	attack_sprite.position = offset
 	attack_sprite.show()
@@ -116,6 +115,8 @@ func bonk_over() -> void:
 			print("Mimic " + str(body) + " has been bonked!")
 			body.get_bonked(attack_strength)
 			break
+	bonking = false
+	$BonkTimer.stop()
 	#this uses signals to hide the attacking nodes after not attacking
 	$AttackBox.monitoring = false
 	$AttackSprite.stop()
