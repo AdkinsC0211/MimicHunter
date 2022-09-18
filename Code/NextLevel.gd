9dd97cc769ab63160c46ffb5f5ebfcf2d6d4a527
ZachWhitten <gumbopaco@gmail.com>
2022-09-18 00:37:09 -0400

    Level Completed Screen

--- Code/NextLevel.gd
+++ Code/NextLevel.gd
@@ -2,13 +2,18 @@ extends Control
 
 var level: int
 func _ready() -> void:
-	pass
+	$NextLevelRect/NextLevel.grab_focus()
 
-
-func do_change_level() -> void:
-	print("changing to level" + str(level+1))
-	get_tree().change_scene("res://Scenes/Levels/Level" + str(level+1) + ".tscn")
+func _process(delta: float) -> void:
+	if visible and Input.is_action_just_pressed("ui_accept"):
+		do_level_change()
 
 func main_menu() -> void:
 	print("fetching main menu")
 	get_tree().change_scene("res://Scenes/Menus/MainMenu.tscn")
+
+
+func do_level_change() -> void:
+	print("changing to level" + str(level+1))
+	get_tree().change_scene("res://Scenes/Levels/Level" + str(level+1) + ".tscn")
+
